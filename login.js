0b2291195ad0145b95699da381efe1c780d35095
t-tsukada1122 <101380234+t-tsukada1122@users.noreply.github.com>
2022-03-12 15:41:47 +0900

    Add files via upload

--- login.js
+++ login.js
@@ -11,7 +11,7 @@ login.addEventListener('click', (e) => {
     if (userID.value === '' || userPassword.value === '') {
         // 入力されていない場合は、メッセージを表示
         message.style.visibility = 'visible';
-    } else if (userID.value === 'nakayama' || userPassword.value === 'kinni-kun') {
+    } else if (userID.value === 'nakayama' && userPassword.value === 'kinni-kun') {
         // 打刻画面へ
         e.preventDefault();
         window.location.href = 'https://t-tsukada1122.github.io/myApp-time/';
